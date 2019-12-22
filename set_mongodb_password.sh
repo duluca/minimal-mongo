50748989221908d2249589839fbefd49d2d11dd0
Doguhan Uluca <duluca@gmail.com>
2019-12-22 17:21:49 -0500

    update to scripts

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -11,23 +11,23 @@ MONGODB_APPLICATION_PASS=${MONGODB_APPLICATION_PASS:-""}
 
 # Wait for MongoDB to boot
 RET=1
-while [[ RET -ne 0 ]] ; do
-    echo "=> Waiting for confirmation of MongoDB service startup..."
-    sleep 5
-    mongo admin --eval "help" >/dev/null 2>&1
-    RET=$?
+while [[ RET -ne 0 ]]; do
+  echo "=> Waiting for confirmation of MongoDB service startup..."
+  sleep 5
+  mongo admin --eval "help" >/dev/null 2>&1
+  RET=$?
 done
 
 if [ "$MONGODB_APPLICATION_DATABASE" != "admin" ]; then
-    echo "=> Creating an ${MONGODB_APPLICATION_DATABASE} user with a password in MongoDB"
-    mongo admin -u $MONGODB_ADMIN_USER -p $MONGODB_ADMIN_PASS << EOF
+  echo "=> Creating an ${MONGODB_APPLICATION_DATABASE} user with a password in MongoDB"
+  mongo admin -u $MONGODB_ADMIN_USER -p $MONGODB_ADMIN_PASS <<EOF
 use $MONGODB_APPLICATION_DATABASE
 db.createUser({user: '$MONGODB_APPLICATION_USER', pwd: '$MONGODB_APPLICATION_PASS', roles:[{role:'dbOwner', db:'$MONGODB_APPLICATION_DATABASE'}]})
 EOF
 
-echo "========================================================================"
-echo "$MONGODB_APPLICATION_USER user created in $MONGODB_APPLICATION_DATABASE! PROTECT the password found in the .env file."
-echo "========================================================================"
+  echo "========================================================================"
+  echo "$MONGODB_APPLICATION_USER user created in $MONGODB_APPLICATION_DATABASE! PROTECT the password found in the .env file."
+  echo "========================================================================"
 else
   echo "=> Skipping App database and user creation."
 fi
