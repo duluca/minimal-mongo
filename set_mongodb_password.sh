7f549b84995c8ebab24306d67426d45bc95262dd
Doguhan Uluca <duluca@gmail.com>
2023-07-31 20:34:10 -0400

    upgraded to 6.0.8

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -14,13 +14,13 @@ RET=1
 while [[ RET -ne 0 ]]; do
   echo "=> Waiting for confirmation of MongoDB service startup..."
   sleep 5
-  mongo admin --eval "help" >/dev/null 2>&1
+  mongosh admin --eval "help" >/dev/null 2>&1
   RET=$?
 done
 
 if [ "$MONGODB_APPLICATION_DATABASE" != "admin" ]; then
   echo "=> Creating an ${MONGODB_APPLICATION_DATABASE} user with a password in MongoDB"
-  mongo admin -u $MONGODB_ADMIN_USER -p $MONGODB_ADMIN_PASS <<EOF
+  mongosh admin -u $MONGODB_ADMIN_USER -p $MONGODB_ADMIN_PASS <<EOF
 use $MONGODB_APPLICATION_DATABASE
 db.createUser({user: '$MONGODB_APPLICATION_USER', pwd: '$MONGODB_APPLICATION_PASS', roles:[{role:'dbOwner', db:'$MONGODB_APPLICATION_DATABASE'}]})
 EOF
